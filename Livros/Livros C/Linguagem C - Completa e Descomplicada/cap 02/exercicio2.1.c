6c3307d6ad9c3e8c8debd68e2cc43d19fd5ecdff
Everton Fernades <evertonfernandesbussiness@gmail.com>
2026-07-12 15:52:55 -0300

    exercicio2.1.c

--- Livros/Livros C/Linguagem C - Completa e Descomplicada/cap 02/exercicio2.1.c
+++ Livros/Livros C/Linguagem C - Completa e Descomplicada/cap 02/exercicio2.1.c
@@ -1,1 +1,9 @@
+// 1) Elabore um programa que escreva as mensagens “Início do programa” e “Fim” na tela, uma em cada linha, usando apenas um comando printf().
 
+#include <stdio.h>
+
+int main(){
+    printf("Inicio do programa\n");
+    printf("Fim\n");
+    return 0;
+}
